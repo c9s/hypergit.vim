d8b6f30ca6b64389c2322e45eca20d9bcf9338ee
c9s <cornelius.howl@gmail.com>
2010-03-24 01:44:46 +0800

    create plugin dir before downloading deps

--- config.mk
+++ config.mk
@@ -4,5 +4,6 @@ VIMRUNTIME=~/.vim
 VERSION=0.7
 
 bundle-deps:
+	mkdir -p plugin
 	$(call fetch_github,c9s,treemenu.vim,master,plugin/treemenu.vim,plugin/treemenu.vim)
 	$(call fetch_github,c9s,helper.vim,master,plugin/helper.vim,plugin/helper.vim)
