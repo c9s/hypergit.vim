1e37fb11a05ee79c06a513a63bad66375353606e
c9s <cornelius.howl@gmail.com>
2010-03-26 19:14:56 +0800

    update config.mk (add rule 'install-github-import'

--- config.mk
+++ config.mk
@@ -7,3 +7,7 @@ bundle-deps:
 	mkdir -p plugin
 	$(call fetch_github,c9s,treemenu.vim,master,plugin/treemenu.vim,plugin/treemenu.vim)
 	$(call fetch_github,c9s,helper.vim,master,plugin/helper.vim,plugin/helper.vim)
+
+
+install-github-import:
+	sudo cpanm Github::Import
